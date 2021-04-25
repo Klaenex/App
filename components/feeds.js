50ee3ac3b9a3deda1d76c8778aac012e7c66c1f8
Klaenex <klaenex.29@gmail.com>
2021-04-25 14:41:19 +0200

    bottomTabs

---
+++ components/feeds.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+
+export default class Feeds extends React.Component {
+  render() {
+    return (
+    <View>
+        <Text>Feeds</Text>
+        
+    </View>
+    );
+  }
+}
