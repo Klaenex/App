436e0aefa956365fde31ef64668519399461afc2
Klaenex <klaenex.29@gmail.com>
2021-04-29 00:17:48 +0200

    save-before-graddle-update

--- components/log.js
+++ components/log.js
@@ -33,26 +33,9 @@ export default class Log extends React.Component {
           underlineColorAndroid="transparent"
           value={this.state.mdp}
         />
-        <Button title='Crée un compte' onPress={}/>
+        <Button title='Crée un compte' onPress={console.log('test')}/>
       </View>
     );
   }
 }
 
-auth(){
-    .createUserWithEmailAndPassword('jane.doe@example.com', 'SuperSecretPassword!')
-    .then(() => {
-      console.log('User account created & signed in!');
-    })
-    .catch(error => {
-      if (error.code === 'auth/email-already-in-use') {
-        console.log('That email address is already in use!');
-      }
-  
-      if (error.code === 'auth/invalid-email') {
-        console.log('That email address is invalid!');
-      }
-  
-      console.error(error);
-    });
-  }
